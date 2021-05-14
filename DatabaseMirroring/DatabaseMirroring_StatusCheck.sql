eac8a40681803feba19b8003b020d91c43db4594
sqlserverdiaries <53661180+sqlserverdiaries@users.noreply.github.com>
2021-05-14 21:19:46 +0100

    New Scripts
    Database Mirroring and Wait Types

---
+++ DatabaseMirroring/DatabaseMirroring_StatusCheck.sql
@@ -0,0 +1,20 @@
+/* Source: https://github.com/reubensultana/DBAScripts/blob/master/DatabaseMirroring/DatabaseMirroring_StatusCheck.sql */
+
+/* NOTE: This script should be run using SQLCMD mode */
+:ON ERROR EXIT
+
+:SETVAR SQLServerInstance "localhost,1433"
+:SETVAR DatabaseName "master"
+
+:CONNECT $(SQLServerInstance)
+USE [$(DatabaseName)]
+GO
+SELECT 
+    d.[name], d.[database_id], dm.[mirroring_role_desc], dm.[mirroring_state_desc], 
+    dm.[mirroring_safety_level_desc], dm.[mirroring_partner_name], dm.[mirroring_partner_instance], 
+    dm.[mirroing_connection_timeout], dm.[mirroring_witness_name], dm.[mirroring_witness_state_desc]
+FROM sys.database_mirroring AS dm
+    INNER JOIN sys.databases d ON dm.[database_id] = d.[database_id]
+WHERE dm.[mirroring_state_desc] IS NOT NULL
+ORDER BY dm.[mirroring_role_desc], d.[name]
+GO
---
+++ System/TopWaits.sql
@@ -0,0 +1,27 @@
+/* Source: https://github.com/reubensultana/DBAScripts/blob/master/System/TopWaits.sql */
+
+/* NOTE: This script should be run using SQLCMD mode */
+:ON ERROR EXIT
+
+:SETVAR SQLServerInstance "localhost,1433"
+:SETVAR DatabaseName "master"
+
+:CONNECT $(SQLServerInstance)
+USE [$(DatabaseName)]
+GO
+SET NOCOUNT ON;
+WITH cteThreads AS (
+    SELECT
+        s.[session_id], r.[command], r.[status], r.[wait_type], r.[scheduler_id], w.[worker_address],
+        w.[is_preemptive], w.[state], t.[task_state], /*t.[session_id],*/ t.[exec_context_id], t.[request_id]
+    FROM sys.dm_exec_sessions AS s
+        INNER JOIN sys.dm_exec_requests AS r ON s.[session_id] = r.[session_id]
+        INNER JOIN sys.dm_os_tasks t ON r.[task_address] = t.[task_address]
+        INNER JOIN sys.dm_os_workers AS w ON t.[worker_address] = w.[worker_address]
+    WHERE s.[is_user_process] = 0
+)
+SELECT [wait_type], COUNT(*) AS [wait_count]
+FROM cteThreads
+GROUP BY [wait_type]
+ORDER BY [wait_count] DESC
+GO
