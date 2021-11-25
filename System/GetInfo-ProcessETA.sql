868031d7aeb006fd453b46cb844d801f30cdffb0
ReubenSultana <20087261+reubensultana@users.noreply.github.com>
2021-11-25 12:26:56 +0000

    More fat finger typos

--- System/GetInfo-ProcessETA.sql
+++ System/GetInfo-ProcessETA.sql
@@ -8,14 +8,14 @@ SELECT
     DB_NAME(er.[database_id]) AS [Database Name],
     er.[command] AS [Command],
     CONVERT(numeric(6,2), er.[percent_complete]) AS [Percent Complete],
-    CONVERT(varechar(20), DATEADD(ms, er.[estimated_completion_time], CURRENT_TIMESTAMP), 20) AS [Estimated Completion Time],
+    CONVERT(varchar(20), DATEADD(ms, er.[estimated_completion_time], CURRENT_TIMESTAMP), 20) AS [Estimated Completion Time],
     CONVERT(numeric(10,2), er.[total_elapsed_time]/1000.0/60.0) AS [Elapsed Min],
     CONVERT(numeric(10,2), er.[total_elapsed_time]/1000.0/60.0/60.0) AS [Elapsed Hours],
     CONVERT(numeric(10,2), er.[estimated_completion_time]/1000.0/60.0) AS [ETA Min],
     CONVERT(numeric(10,2), er.[estimated_completion_time]/1000.0/60.0/60.0) AS [ETA Hours],
     CONVERT(varchar(1000), (
         SELECT SUBSTRING(est.[text], er.[statement_start_offset]/2, 
-            CASE WHEN er.[statemend_end_offset] = -1 THEN 1000 ELSE (er.[statement_emd_offset] - er.[statement_start_offset])/2 END)
+            CASE WHEN er.[statement_end_offset] = -1 THEN 1000 ELSE (er.[statement_end_offset] - er.[statement_start_offset])/2 END)
             FROM sys.dm_exec_sql_text(er.[sql_handle]) est
         )) AS [SQL Text]
 FROM sys.dm_exec_requests er
