33db493bc641a7e1448aa194ce1af489fde45d3e
ReubenSultana <20087261+reubensultana@users.noreply.github.com>
2022-05-13 18:34:55 +0100

    Missed that

--- PowerShell/Get-SqlServerVersions.ps1
+++ PowerShell/Get-SqlServerVersions.ps1
@@ -2,19 +2,20 @@
 
 # NOTE: The information retrieved is based on data https://www.sqlserverversions.com/
 
+Add-Type -Assembly System.Web # [System.Web.HttpUtility]::UrlEncode() needs this
 
 # retrieve all versions, service packs, patches, hotfixes, updates, etc. (6.0 to date)
-$Query = "select *"
-$URL   = "https://docs.google.com/spreadsheets/d/16Ymdz80xlCzb6CwRFVokwo0onkofVYFoSkc7mYe6pgw/gviz/tq?tq=" + [System.Web.HttpUtility]::UrlEncode($Query) + "&tqx=out:csv"
+[string] $Query = "select *"
+[string] $URL   = "https://docs.google.com/spreadsheets/d/16Ymdz80xlCzb6CwRFVokwo0onkofVYFoSkc7mYe6pgw/gviz/tq?tq=" + [System.Web.HttpUtility]::UrlEncode($Query) + "&tqx=out:csv"
 
 $SqlServerVersions = Invoke-WebRequest $URL
 $SqlServerVersions.Content | ConvertFrom-Csv | Out-GridView
 
 
 # same as above, however limiting results to the 2017 version
-$SqlVersion = "2017"
-$Query = "select * where A='" + $SqlVersion + "'"
-$URL   = "https://docs.google.com/spreadsheets/d/16Ymdz80xlCzb6CwRFVokwo0onkofVYFoSkc7mYe6pgw/gviz/tq?tq=" + [System.Web.HttpUtility]::UrlEncode($Query) + "&tqx=out:csv"
+[string] $SqlVersion = "2017"
+[string] $Query = "select * where A='" + $SqlVersion + "'"
+[string] $URL   = "https://docs.google.com/spreadsheets/d/16Ymdz80xlCzb6CwRFVokwo0onkofVYFoSkc7mYe6pgw/gviz/tq?tq=" + [System.Web.HttpUtility]::UrlEncode($Query) + "&tqx=out:csv"
 
 $SqlServerVersions = Invoke-WebRequest $URL
 $SqlServerVersions.Content | ConvertFrom-Csv | Out-GridView
