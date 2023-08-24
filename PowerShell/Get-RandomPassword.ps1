c5bbf11e8ca427a0fe7182fb81d73296d62aa156
ReubenSultana <20087261+reubensultana@users.noreply.github.com>
2023-08-24 12:23:42 +0100

    Additional input parameters

--- PowerShell/Get-RandomPassword.ps1
+++ PowerShell/Get-RandomPassword.ps1
@@ -4,16 +4,35 @@ function Get-RandomPassword {
     [CmdletBinding()]
     param (
         [Parameter(Position=0, Mandatory=$true)] [int] $OutputLength
+        ,
+        [Parameter(Position=1, Mandatory=$false)] [bool] $UseUpper = $true
+        ,
+        [Parameter(Position=2, Mandatory=$false)] [bool] $UseLower = $true
+        ,
+        [Parameter(Position=3, Mandatory=$false)] [bool] $UseNumbers = $true
+        ,
+        [Parameter(Position=4, Mandatory=$false)] [bool] $UseSymbols = $true
+        ,
+        [Parameter(Position=5, Mandatory=$false)] [string] $OverrrideSymbols = $null
     )
     
     # check and default to 12
     if ($null -eq $OutputLength -or $OutputLength -lt 8) { $OutputLength = 12 }
+    
+    # check that at least one category has been selected
+    if (($false -eq $UseUpper) -and ($false -eq $UseLower) -and ($false -eq $UseNumbers) -and ($false -eq $UseSymbols)) { 
+        Write-Error "You cannot exclude all categories. The best option is to omit values and stick to the defaults"
+        Return $null
+    }
 
     # arrays of alowed characters
-    $UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()
-    $LowerCase = "abcdefghijklmnopqrstuvwxyz".ToCharArray()
-    $Numbers = "0123456789".ToCharArray()
-    $Symbols = "!@#$%^&*()-_=+{}[]|\:;<>,.?/".ToCharArray()
+    if ($true -eq $UseUpper) { $UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray() } else { $UpperCase = $null }
+    if ($true -eq $UseLower) { $LowerCase = "abcdefghijklmnopqrstuvwxyz".ToCharArray() } else { $LowerCase = $null }
+    if ($true -eq $UseNumbers) { $Numbers = "0123456789".ToCharArray() } else { $Numbers = $null }
+    if ($true -eq $UseSymbols) {
+        if (($null -eq $OverrrideSymbols) -or ($OverrrideSymbols.Length -lt 1)) { $Symbols = "!@#$%^&*()-_=+{}[]|\:;<>,.?/".ToCharArray() }
+        else { $Symbols = $OverrrideSymbols.ToCharArray() }
+    } else { $Symbols = $null }
 
     # build a single array of all characters
     $AllowedCharacters = $UpperCase + $LowerCase + $Numbers + $Symbols
@@ -24,7 +43,7 @@ function Get-RandomPassword {
     if ($OutputLength -gt $MaxLength) { $OutputLength = $MaxLength }
 
     # build the password
-    [string] $Password = ($AllowedCharacters | Get-Random -Count $OutputLength) -join ''
+    [string] $Password = ($AllowedCharacters | Get-Random -Count $OutputLength -ErrorAction SilentlyContinue) -join ''
     
     Return $Password
 }
